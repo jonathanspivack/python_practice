9bc2571a867f2ecac6d14989c0e24f42369dbe73
Jonathan Spivack <34500957+jonathanspivack@users.noreply.github.com>
2018-05-24 00:07:50 -0400

    Create delete_duplicates_linkedlist.py

---
+++ delete_duplicates_linkedlist.py
@@ -0,0 +1,8 @@
+def RemoveDuplicates(head):
+    cur = head
+    while cur.next:
+        if cur.data == cur.next.data:
+            cur.next = cur.next.next
+        else:
+            cur = cur.next
+    return head
